eff439ff7950774501efe2f523bbd4e2144eccb1
Carot19 <rvt.sim19@gmail.com>
2021-04-06 20:22:06 +0200

    Documentation fichier,

--- Calculatrice/Calculatrice.py
+++ Calculatrice/Calculatrice.py
@@ -1,5 +1,11 @@
-# L'importation de l’ensemble des éléments du paquet tkinter :
+#Calculatrice
+#Author: RIVET Simon
+#date : 06/04/2021
+#Version : 0.0.1
+#Description : entrainement python et git, réalisation d'une calculatrice"
 
+
+# L'importation de l’ensemble des éléments du paquet tkinter :
 from tkinter import *
 
 # Création d'une fenêtre avec la classe Tk :
